7f54e3ebd54b4940a2150c7c2061d0fc9c47e1e5
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-02-22 14:13:05 +0530

    Pre-Prisma setup final commit

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,10 +1,8 @@
 import useLoginModal from '@/hooks/useLoginModal';
-import { useRouter } from 'next/router'
 import React, { useCallback } from 'react'
 import { FaFeather } from 'react-icons/fa';
 
 const SidebarTweetButton = () => {
-    const router = useRouter();
     const loginModal = useLoginModal();
 
     const onClick = useCallback(() => {
