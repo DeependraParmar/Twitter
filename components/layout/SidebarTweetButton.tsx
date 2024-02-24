1eafba439856d91d21a7f44116370124b15b543f
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-02-24 15:39:39 +0530

    Almost done with this

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -17,12 +17,12 @@ const SidebarTweetButton = () => {
     }, [loginModal, router, currentUser]);
 
   return (
-    <div onClick={onClick}>
-        <div className="mt-6 lg:hidden rounded-full h-12 w-12 p-4 flex items-center
+    <div onClick={onClick} className='flex justify-center items-center md:w-full'>
+        <div className="mt-6 lg:hidden  rounded-full h-12 w-12 p-4 flex items-center
          justify-center bg-sky-500 hover:bg-opacity-80 cursor-pointer transition">
             <FaFeather size={24} color="white" />
          </div>
-         <div className='mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-90 cursor-pointer transition'>
+         <div className='mt-6 hidden lg:block w-full px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-90 cursor-pointer transition'>
             <p className="hidden lg:block text-center font-semibold text-white text-[20px]">Tweet</p>
          </div>
     </div>
